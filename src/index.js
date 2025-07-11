6c8bbdfee9e4d856c88b3eefe9dbe89a8dc87804
Sahil Deore <sahildeore1234@gmail.com>
2025-07-11 09:00:19 +0530

    Refactor App component and add Header, Menu, Footer
    Split the App component into Header, Menu, and Footer components for better structure. Added logic in Footer to determine if the restaurant is open based on the current hour. Updated the main render to use these new components.

--- src/index.js
+++ src/index.js
@@ -49,12 +49,38 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React!!!!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
   );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+  //if (hour >= openHour && hour <= closeHour) alert("We're Currently Open!");
+  //else alert("Sorry we're closed");
+  return <footer>We're Currently Open</footer>;
 }
 
 function Pizza() {
