4777638f4603ff75a964ccdef11d7139848053f5
Sahil D <114382046+Sahildk@users.noreply.github.com>
2025-07-13 04:03:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -93,7 +93,7 @@ function Pizza(props) {
   return (
     <div className="pizza">
       <img src={props.photoName} alt={props.name}></img>
-      <div className="pizza">
+      <div>
         <h3>{props.name}</h3>
         <p>{props.ingredients}</p>
         <span>{props.price}</span>
