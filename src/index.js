2954fb0a72fe43e797f4a52c96d246dc521c2bb3
Sahil D <114382046+Sahildk@users.noreply.github.com>
2025-07-13 03:11:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -72,11 +72,33 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        photoName="pizzas/funghi.jpg"
+        price={12}
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  return (
+    <div>
+      <img src={props.photoName} alt={props.name}></img>
+      <div className="pizza">
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
   );
 }
 
@@ -89,16 +111,6 @@ function Footer() {
   //if (hour >= openHour && hour <= closeHour) alert("We're Currently Open!");
   //else alert("Sorry we're closed");
   return <footer className="footer">We're Currently Open</footer>;
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci"></img>
-      <h3>Pizza Spinaci</h3>
-      <p>'Tomato, mozarella, spinach, and ricotta cheese'</p>
-    </div>
-  );
 }
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
